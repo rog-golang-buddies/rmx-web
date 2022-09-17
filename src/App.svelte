a0466dd828861809cecccba9f58b835e54e3e510
pmoieni <pmoieni06@gmail.com>
2022-09-17 21:47:06 +0430

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { Route, Router } from "svelte-navigator";
+  import Nav from "./lib/Nav.svelte";
+  import Home from "./pages/Home.svelte";
+</script>
+
+<main>
+  <Router>
+    <Nav />
+    <Route path="/">
+      <Home />
+    </Route>
+  </Router>
+</main>
+
+<style>
+  :global(*) {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+</style>
---
+++ src/lib/Nav.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import { Link } from "svelte-navigator";
+</script>
+
+<nav>
+  <div class="logo">RMX</div>
+  <div class="nav-items">
+    <Link to="/">Home</Link>
+    <Link to="/learn">Learn</Link>
+  </div>
+  <div class="nav-options" />
+</nav>
+
+<style lang="scss">
+  nav {
+    width: 5rem;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    box-shadow: 0px 0px 10px rgba($color: #000000, $alpha: 0.3);
+
+    & > div {
+      width: 100%;
+    }
+  }
+</style>
