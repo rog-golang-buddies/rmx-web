08a2f1ade4d0ad69e9e3fb6cb309cdf8444cb23a
Harvey Sanders <sanders.harvey@gmail.com>
2023-01-01 08:52:03 -0500

    Add env for api config

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const { VITE_RMX_API_BASE } = import.meta.env;
+
 export const api = axios.create({
-    baseURL: 'http://localhost:8888/api/v1',
+    baseURL: VITE_RMX_API_BASE ?? 'http://localhost:8888/api/v1',
     headers: {
         'Content-Type': 'application/json',
     },
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { defineConfig, loadEnv } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svelte()]
-})
+export default defineConfig(({ command, mode }) => {
+    // Load env file based on `mode` in the current working directory.
+    // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+    const env = loadEnv(mode, process.cwd(), '');
+
+    // vite config
+    return {
+        plugins: [svelte()],
+        define: {
+            __APP_ENV__: env.APP_ENV,
+        },
+    };
+});
